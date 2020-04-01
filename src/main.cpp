0dfd777b618ca27e1e39d57e498b7f04906ef4d3
Michael <michael.aug@protonmail.com>
2020-04-01 23:08:02 +0100

    Initialise, Create and clean up GLFW window

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,61 @@
-#include <vulkan/vulkan.h>
-
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
+        InitWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    GLFWwindow* window;
+
+    void InitWindow() {
+
+        // Init GLFW library
+        glfwInit();
+
+        // Tell GLFW not to create OpenGL context
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+        // Disable screen resizing (can't handle resizable windows for now)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); 
+
+        //parameters: width, height, title, monitor, OpenGL parameter
+        window = glfwCreateWindow(WIDTH,HEIGHT, "Vulkan", nullptr, nullptr);
+
+        std::cout<<"vulkan supported: "<<glfwVulkanSupported()<<std::endl;
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
     }
 };
 
 int main() {
     HelloTriangleApplication app;
-    std::cout<<"Hello Vulkan World!" <<std::endl;
     try {
         app.run();
     } catch (const std::exception& e) {
